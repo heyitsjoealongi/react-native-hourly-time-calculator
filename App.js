393fb8be03bfa20f630da757bb552be6cfcf6ffc
Joe Alongi <collectedview@gmail.com>
2021-06-12 18:01:29 -0400

    Template Reset

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { SafeAreaView, View, Text } from "react-native";
-
-import Form from "./components/form";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <SafeAreaView>
-        <View>
-          <Text>Calculate Your Hours</Text>
-          <Form />
-        </View>
-      </SafeAreaView>
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
