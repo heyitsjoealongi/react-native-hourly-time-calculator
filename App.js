91d84b2aa29dfb2eeb39870cc98bb8b6150fc359
Joe Alongi <collectedview@gmail.com>
2021-06-12 23:36:47 -0400

    App Framework

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 import TimeField from "react-simple-timefield";
 
@@ -66,46 +66,71 @@ class App extends React.Component {
 
     return (
       <View style={styles.container}>
-        <Text>Calculate Your Hours</Text>
-        <TextInput
-          value={timeTotal}
-          onChange={this.onTimeTotalChange}
-          style={{
-            border: "2px solid #666",
-            fontSize: 42,
-            width: 200,
-            padding: "5px 8px",
-            margin: "8px auto",
-            color: "#333",
-            borderRadius: 3,
-          }}
-        />
-        <TimeField
-          value={timeOne}
-          onChange={this.onTimeOneChange}
-          style={{
-            border: "2px solid #666",
-            fontSize: 42,
-            width: 200,
-            padding: "5px 8px",
-            margin: "8px auto",
-            color: "#333",
-            borderRadius: 3,
-          }}
-        />
-        <TimeField
-          value={timeTwo}
-          onChange={this.onTimeTwoChange}
-          style={{
-            border: "2px solid #666",
-            fontSize: 42,
-            width: 200,
-            padding: "5px 8px",
-            margin: "8px auto",
-            color: "#333",
-            borderRadius: 3,
-          }}
-        />
+        <View style={styles.calculator}>
+          <View style={styles.totalGroup}>
+            <Text
+              style={{
+                fontSize: 42,
+                textAlign: "center",
+                padding: "24px",
+                margin: "24px auto",
+                color: "#333",
+              }}
+            >
+              Calculate Your Hours
+            </Text>
+            <TextInput
+              value={timeTotal}
+              onChange={this.onTimeTotalChange}
+              style={{
+                border: "2px solid #666",
+                fontSize: 42,
+                textAlign: "center",
+                width: "100%",
+                padding: "8px",
+                margin: "0 0 8px 0",
+                color: "#333",
+                borderRadius: 3,
+              }}
+              disabled
+            />
+          </View>
+          <View style={styles.inputGroup}>
+            <TimeField
+              value={timeOne}
+              onChange={this.onTimeOneChange}
+              style={{
+                border: "2px solid #666",
+                fontSize: 42,
+                textAlign: "center",
+                height: "48px",
+                width: "50%",
+                padding: "8px",
+                margin: "8px 0 8px 0",
+                color: "#333",
+                borderRadius: 3,
+              }}
+            />
+            <TimeField
+              value={timeTwo}
+              onChange={this.onTimeTwoChange}
+              style={{
+                border: "2px solid #666",
+                fontSize: 42,
+                textAlign: "center",
+                height: "48px",
+                width: "50%",
+                padding: "8px",
+                margin: "8px 0 8px 8px",
+                color: "#333",
+                borderRadius: 3,
+              }}
+            />
+          </View>
+          <View style={styles.buttonGroup}>
+            <Button title="Calculate" />
+          </View>
+        </View>
       </View>
     );
   }
@@ -115,8 +140,46 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  calculator: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    padding: "64px",
+    margin: "64px",
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    border: "2px solid #666",
+    maxWidth: 600,
+  },
+  totalGroup: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    maxHeight: 200,
+    width: "100%",
+  },
+  inputGroup: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    maxHeight: 100,
+    width: "100%",
+  },
+  buttonGroup: {
+    flex: 1,
+    flexDirection: "column",
+    maxHeight: 100,
+    width: "100%",
   },
 });
